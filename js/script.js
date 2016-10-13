165442929084965850e860e15b6f018081957758
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2016-10-13 14:39:36 +0100

    Added a description of tasks and opened a new object

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@
 /*global
     console, __
 */
+(function () {
+    'use strict';
+    /*
+    - How I intend for this branch to work
+    * The Model/Store will keep track of the Data with the following methods - 
+        * init
+        * create
+        * save
+        * find
+    * The View will manage the DOM like so -
+        * init
+        * removeEl
+        * renderOne
+        * renderAll
+    * The Controller will glue the above by -
+        * init and binding
+    */
+    var _ = document.querySelector.bind(document),
+        todoList = {};
+    
+}());
