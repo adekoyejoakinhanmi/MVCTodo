73c2517b9a867eed2285b896f788116c217b60fa
adekoyejoakinhanmi <koyesavedbygrace@yahoo.com>
2017-11-13 13:45:31 +0100

    add proper code comments

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
-/* @todo :
- * 1. Rename methods properly
- * 2. Destructure view's data
- * 3. Add disabled prop when there are no completed todos
- * 4. Add localStorage
- */ 
+
+
+/**
+ * Will return DOM node from given string
+ * 
+ * @param {String} html 
+ */
 function htmlToDOM(html) {
     let parser = new DOMParser();
     let doc = parser.parseFromString(html, "text/html");
     return doc.body.firstChild;
 }
+
+/**
+ * Find and return the parent node of a given
+ * tag
+ * 
+ * @param {HTMLElement} element 
+ * @param {String} tagName 
+ */
 
 function $parent(element, tagName) {
     if (!element.parentNode) {
@@ -20,89 +29,110 @@ function $parent(element, tagName) {
     return $parent(element.parentNode, tagName);
 }
 
-//===============================
 
+/**
+ * Create a new todo object
+ * 
+ * @param {Object} attrs
+ */
 class Todo {
     constructor(attrs) {
         for (var attr in attrs) {
             this[attr] = attrs[attr]
         }
     }
+    /**
+     * Toggle the completed state of the todo
+     */
     toggleCompleted() {
         this.completed = !this.completed
     }
 }
 
+/**
+ * Creates a new collection to manage the todo list
+ */
 class TodoCollection {
     constructor() {
-        /* @todo: find a way to initialize external data. */
-        this.$todos = [];
+        this.todos = [];
     }
-    /* Unique ID helper */
-    cid() {
+    /**
+     * ID helper of this collection
+     */
+    $cid() {
         return Math.random().toString(16).slice(2);
     }
-    getById(id) {
-        return this.$todos.map(t => t.id).indexOf(id);
+    /**
+     * Finds the index of a given todo in the collection
+     * based on the internal id of the todo
+     * 
+     * @param {String} id 
+     */
+    $getById(id) {
+        return this.todos.map(t => t.id).indexOf(id);
     }
-    // A default gateway for all actions possible 
-    // on this collection. 
-    $set(action, prop) {
-        if (action === 'add') {
-            this.add(prop);
-        }
-        if (action === 'remove') {
-            this.remove(prop);
-        }
-        if (action === 'update') {
-            this.update(prop);
-        }
-        if (action === 'clearCompleted') {
-            this.clearComplete();
-        }
-    }
-    add(title) {
-        let thisId = this.cid();
+    /**
+     * Adds a new todo to the collection
+     * 
+     * @param {String} title 
+     */
+    _add(title) {
+        let thisId = this.$cid();
         let todo = {
             id: thisId,
             title: title,
             completed: false
         };
-        this.$todos.push(new Todo(todo));
+        this.todos.push(new Todo(todo));
     }
-    remove(id) {
-        /* Remove Object from list */
-        let idx = this.getById(id);
-        this.$todos.splice(idx, 1);
-    }
-    update(id) {
-        let idx = this.getById(id);
-        this.$todos[idx].toggleCompleted();
-    }
-    /* This mutates the internal data
-     * but then I am not sure of any other
-     * way to remove all completed
+    /**
+     * Removes a todo from the collection based on 
+     * given id
+     * 
+     * @param {String} id 
      */
-    clearComplete() {
-        let list = this.$todos;
-        this.$todos = list.filter(t => !t.completed);
+    _remove(id) {
+        let idx = this.$getById(id);
+        this.todos.splice(idx, 1);
     }
-    // A default gateway for all requests possible
-    // on this collection.
-    $get(req) {
-        let activeList = this.$todos.filter(t => !t.completed);
-        let completedList = this.$todos.filter(t => t.completed);
+    /**
+     * Updates the completed state of a todo in the collection
+     * 
+     * @param {String} id 
+     */
+    _update(id) {
+        let idx = this.$getById(id);
+        this.todos[idx].toggleCompleted();
+    }
+    /**
+     * Filters the collection for completed items
+     */
+    _clearComplete() {
+        let list = this.$todos;
+        this.todos = list.filter(t => !t.completed);
+    }
+    /**
+     * Gives access to retrive data from the collection 
+     * based on req
+     * 
+     * @param {String} req 
+     * @returns {Object} state which represents all possible 
+     * data from this collection
+     */
+    get(req) {
+        let activeList = this.todos.filter(t => !t.completed);
+        let completedList = this.todos.filter(t => t.completed);
 
         let state = {
             list : [],
             count : {
                 active : activeList.length,
-                total : this.$todos.length,
+                total : this.todos.length,
                 completed : completedList.length
             }
         }
         if (req === 'all') {
-            state.list = this.$todos;
+            state.list = this.todos;
         } else if (req === 'completed') {
             state.list = completedList
         } else if (req === 'active') {
@@ -111,16 +141,52 @@ class TodoCollection {
 
         return state;
     }
+    /**
+     * Provides a way to perform actions on the collection.
+     * 
+     * @param {String} action The name of the action to be performed
+     * @param {String|Object} prop The data to perform the action with
+     * 
+     * @example
+     * set('add', {todo}) // Will add a new todo to the collection
+     * set('remove', todoId) // Will remove the todo with the given todoId 
+     * set('update', todoId) // Will update the todo with the given todoId
+     * set('clearCompleted') // Will remove completed todos
+     */
+    set(action, prop) {
+        if (action === 'add') {
+            this._add(prop);
+        }
+        if (action === 'remove') {
+            this._remove(prop);
+        }
+        if (action === 'update') {
+            this._update(prop);
+        }
+        if (action === 'clearCompleted') {
+            this._clearComplete();
+        }
+    }
 }
 
+/**
+ * View that deals with DOM. It provides two
+ * render functions.
+ *  1. ListRender -> which renders the list using a HTML document frag
+ *                   to implement faster renders
+ *  2. InfoRender -> which renders the info bar
+ */
 class View {
     constructor() {
-        this.$viewState = 'all';
+        this.viewState = 'all';
         this._listFrag = document.createDocumentFragment();
 
-        this.$loadDOM();
+        this._loadDOM();
     }
-    $loadDOM() {
+    /**
+     * Load the DOM
+     */
+    _loadDOM() {
         this.$list = document.getElementById('todo-list');
         this.$input = document.getElementById('newTodo');
         this.$infoBar = document.getElementById('info-bar');
@@ -131,6 +197,11 @@ class View {
         this.$infoBar.classList.remove('hidden');
         this.$infoBar.innerHTML = '';
     }
+    /**
+     * Todo Item template
+     * @param {Object} param0 Destructures todo object
+     * @returns {HTMLElement} 
+     */
     $todoTmpl({id, title, completed}) {
         let item = `
             <li data-id="${id}" class="${completed ? 'completed' : ''}">
@@ -143,10 +214,13 @@ class View {
         `;
         return htmlToDOM(item);
     }
-    $filterTmpl({ count : { active : a, total : t, completed : c} }) {
-        //let active = count['active'];
+    /**
+     * Info bar template
+     * @param {Object} param0 Destructes count object from given state object 
+     */
+    $infoTmpl({ count : { active : a, total : t, completed : c} }) {
         let hidden = t <= 0 ? true : false;
-        let state = this.$viewState;
+        let state = this.viewState;
         
         let tmpl = `
         <div class="${hidden ? 'hidden' : ''}">
@@ -163,14 +237,22 @@ class View {
         `;
         return htmlToDOM(tmpl);
     }
+    /**
+     * Helps the list render function
+     * 
+     * @param {Object} item todo data to be render
+     */
     $listHelper(item) {
         let node = this.$todoTmpl(item);
         this._listFrag.appendChild(node);
     }
+    /**
+     * Render list
+     * @param {Object} param0 Destructures list object from given state
+     */
     listRender({ list }) {
         // Clear this list
         this._listFrag.innerHTML = '';
-
         // If data passed is empty update accordingly
         if (!list || list.length === 0) {
             let p = document.createElement('p');
@@ -178,21 +260,18 @@ class View {
             p.innerText = 'Nothing to see here';
             this._listFrag.appendChild(p);
         }
-
         // Otherwise insert nodes for each data into frag
         else {
             for (var i = 0, l = list.length; i < l; i++) {
                 this.$listHelper(list[i]);
             }
         }
-
         // Finally, update DOM with frag list
         this.$list.innerHTML = '';
         this.$list.appendChild(this._listFrag);
     }
     infoRender(data) {
-        let infoBar = this.$filterTmpl(data);
-
+        let infoBar = this.$infoTmpl(data);
         // Update DOM
         this.$infoBar.innerHTML = '';
         this.$infoBar.appendChild(infoBar);
@@ -201,27 +280,36 @@ class View {
     clearInput() {
         this.$input.value = '';
     }
+    /**
+     * Changes view state 
+     * @param {String} newState 
+     */
     changeViewState(newState) {
-        /* @todo: add validation to prevent unavailable state */
-        this.$viewState = newState;
-        
+        this.viewState = newState;  
     }
 }
-
+/**
+ * Takes a view and a collection and act as the controller within them
+ * 
+ * @constructor
+ * @param {Object} view The View Instance
+ * @param {Object} collection The Collection instance
+ */
 class Controller {
     constructor(view, collection) {
         this.view = view;
         this.collection = collection;
-
-        this.$events();
-
+        // Initialize events
+        this._initEvents();
         // Initial list render
         this.view.listRender({ list : [] });
     }
-    $events() {
+    /**
+     * Bind all possible events on view
+     */
+    _initEvents() {
         this.view.$input.addEventListener('keypress', (e) => {
             let val = this.view.$input.value;
-
             /* Prevent empty strings */
             if (!val.trim()) {
                 return;
@@ -231,7 +319,7 @@ class Controller {
             }
         });
         this.view.$list.addEventListener('click', (e) => {
-            let id = this._getId(e.target);
+            let id = this.$getId(e.target);
 
             if (e.target.matches('.remove')) {
                 this._remove(id);
@@ -255,46 +343,74 @@ class Controller {
             }
         })
     }
-    _add(title) {
-        this.collection.$set('add', title);
-
-        this._viewUpdate();
-        this.view.clearInput();
-    }
-    _remove(id) {
-        this.collection.$set('remove', id);
-        this._viewUpdate();
-    }
-    _update(id) {
-        this.collection.$set('update', id);
-        this._viewUpdate();
-    }
-    _clearCompleted() {
-        this.collection.$set('clearCompleted');
-        this._viewUpdate();
-    }
-    _getId(el) {
+    /**
+     * Get the parent's id of a given html element
+     * 
+     * @param {HTMLElement} el 
+     */
+    $getId(el) {
         let item = $parent(el, 'li');
         return String(item.dataset.id);
     }
+    /**
+     * Add a particular todo
+     * 
+     * @param {String} title 
+     */
+    _add(title) {
+        this.collection.set('add', title);
+
+        this._updateView();
+        this.view.clearInput();
+    }
+    /**
+     * Remove a particlar todo
+     * 
+     * @param {String} id 
+     */
+    _remove(id) {
+        this.collection.set('remove', id);
+        this._updateView();
+    }
+    /**
+     * Update a todo item
+     * 
+     * @param {String} id 
+     */
+    _update(id) {
+        this.collection.set('update', id);
+        this._updateView();
+    }
+    /**
+     * Clear completed todos
+     */
+    _clearCompleted() {
+        this.collection.set('clearCompleted');
+        this._updateView();
+    }
+    /**
+     * Changes the value of the view state
+     * @param {String} val 
+     */
     _updateViewState(val) {
         this.view.changeViewState(val);
-        this._viewUpdate();
+        this._updateView();
     }
-    _viewUpdate() {
-        let viewState = this.view.$viewState;
+    /**
+     * Update the view by calling view's render functions
+     */
+    _updateView() {
+        let viewState = this.view.viewState;
         let $view = this.view;
         let state;
 
         if (viewState === 'all') {
-            state = this.collection.$get('all');
+            state = this.collection.get('all');
         } else if (viewState === 'completed') {
-            state = this.collection.$get('completed');
+            state = this.collection.get('completed');
         } else if (viewState === 'active') {
-            state = this.collection.$get('active');
+            state = this.collection.get('active');
         }
-
-        /* @todo: send only needed data  */
         $view.listRender(state);
         $view.infoRender(state);
     }
